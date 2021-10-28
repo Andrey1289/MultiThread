6cbef0b50f26cedada8973b8fae5118cfa1008d5
Andrey Galkovskii <dobpuha@gmail.com>
2021-10-28 15:28:33 +0700

    Shame

--- src/main/java/com/andrey/Foo.java
+++ src/main/java/com/andrey/Foo.java
@@ -1,56 +1,50 @@
 package main.java.com.andrey;
-
 import java.sql.SQLOutput;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Foo {
-    public void first(Runnable r){
-        System.out.println("first");
+public  class Foo  {
+    public static  boolean flag = false;
+    public static  int n = 0;
+     static Thread A = new Thread(()->{
+         first();
+        flag = true;
+     });
+   static Thread B = new Thread(()->{
+       try {
+           Thread.sleep(1000);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+       if (flag==true ){
+           second();
+       n =1;}
+   });
+   static Thread C = new Thread(()->{
+       try {
+           Thread.sleep(2000);
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+       if(flag == true && n ==1)
+           third();
+   });
+  public  static void first(){
+     System.out.println("first");
     }
-    public void second(Runnable r){
-        System.out.println("second");
+    public static void second(){
+      System.out.println("second");
     }
-    public void third(Runnable r){
+    public static void third(){
         System.out.println("third");
+    }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Foo foo = new Foo();
+       C.start();
+       B.start();
+       A.start();
+    }
+}
 
-    }
-}
-class test{
-    public static void main(String[] args) {
-        ExecutorService es = Executors.newFixedThreadPool(3);
-        CyclicBarrier cb = new CyclicBarrier(3, new BarAction());
-       es.execute(new MyThread(cb,"C"));
-        es.execute(new MyThread(cb,"A"));
-        es.execute(new MyThread(cb,"B"));
 
-        es.shutdown();
-    }
-}
-class MyThread implements Runnable{
-    CyclicBarrier cb;
-    String name;
-    MyThread(CyclicBarrier c, String n){
-        cb = c;
-         name = n;
-        new Thread(this);
-    }
-    public void run(){
-        try {
-            cb.await();
-        }catch (BrokenBarrierException e){
-            System.out.println(e);
-        }catch (InterruptedException e){
-            System.out.println(e);
-        }
-    }
-}
-class  BarAction implements Runnable{
-Foo f = new Foo();
-    @Override
-    public void run() {
-      f.first(this::run);
-        f.second(this::run);
-        f.third(this::run);
-
-    }
-}
